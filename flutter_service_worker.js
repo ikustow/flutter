2f6ff5e6ab00664436b0a802ccd40094d0e36c1d
ikustow <36426291+ikustow@users.noreply.github.com>
2022-08-17 22:52:36 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "70aebd8903b2fca5daa774d86fe02577",
-"/": "70aebd8903b2fca5daa774d86fe02577",
-"main.dart.js": "0c79e0ba17568bd26850ce1f5ce95d7c",
+"index.html": "12c58817db9bc4cd01fea0038bc2b14c",
+"/": "12c58817db9bc4cd01fea0038bc2b14c",
+"main.dart.js": "4a1c6970e73a2ca660571e0f342928d7",
 "manifest.json": "d40c47d1c161f94dbcb13094d37f1f55",
 "version.json": "009c9e65172e010890f7f65fde438006"
 };
